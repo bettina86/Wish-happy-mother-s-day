789e51fa5b45d83e30787adb78b350c0542c46ef
bettina86 <bettina86@users.noreply.github.com>
2018-05-12 22:39:41 +0200

    Create mothersday.cpp

---
+++ mothersday.cpp
@@ -0,0 +1,67 @@
+#include<windows.h>
+#include<iostream>
+#include <mmsystem.h>
+using namespace std;
+
+int main()
+{
+
+ int a;
+ int i;
+ char h = 3;
+ system("cls");
+ system("color f5");
+ cout << "\t\t\t    Da  : Elisabetta\n\n\a";
+ Sleep(1000);
+ cout << "\t\t\t    A  : Mamma\n\n\a";
+ Sleep(1000);
+ cout << "\t\t";
+ for (i = 0; i<46; i++)
+  cout << "_";
+ cout << "\n\n\a";
+
+ Sleep(1000);
+ cout << "\t\t\t\t  .....  .....\n\a\a";
+
+ Sleep(1000);
+ cout << "\t\t\t\t....... .......\n\a\a\a";
+
+ Sleep(1000);
+ cout << "\t\t\t       .................\n\a";
+
+ Sleep(1000);
+ cout << "\t\t\t      |" << h << "  13/05/2018  " << h << "|\n\a";
+
+ Sleep(1000);
+ cout << "\t\t\t      |" << h << "              " << h << "|\n\a";
+
+ Sleep(1000);
+ cout << "\t\t\t        |__ " << h << "" << h << "   " << h << "" << h << " __|\n\a";
+
+ Sleep(1000);
+ cout << "\t\t\t          |__HAPPY__|\n\a";
+ 
+ Sleep(1000);
+ cout << "\t\t\t       .................\n\a";
+
+ Sleep(1000);
+ cout << "\t\t               |_ Mother's Day_|\n\a";
+
+ Sleep(1000);
+ cout << "\t\t                     |"  << h << "  |\n\a";
+
+
+ Sleep(1000);
+ cout << "\t\t                      |_|\n\a";
+
+ cout << "\n\n\n\n\n";
+ Sleep(1000);
+
+ for (int a = 1; a < 13; a++)
+ {
+  cout << "Buona festa dellla Mamma" << endl << endl;
+  Sleep(100);
+ }
+ system("pause");
+ return 0;
+}
